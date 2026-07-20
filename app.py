f3d4c64f4ee4bfcc66f94f3cd4ec7524768d5796
dkrajouria22-lab <dkrajouria22@gmail.com>
2026-07-20 14:11:13 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.title("Voting Eligibility Checker 🗳️")
+
+age = st.number_input("Enter your age:", min_value=0, max_value=120, value=18)
+
+if st.button("Check Eligibility"):
+    if age >= 18:
+        st.success("You are eligible for vote")
+        vote = st.radio("Choose party:", ["BJP", "CJP"])
+        if vote == "BJP":
+            st.write("PM Modi")
+    else:
+        st.error("You are not eligible for vote")
+      
